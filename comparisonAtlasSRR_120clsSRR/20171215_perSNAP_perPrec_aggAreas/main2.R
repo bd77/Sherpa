dffcbefff0c7c5339ece3db248f827fc9a6cf7be
enricopisoni <enrico.pisoni@gmail.com>
2017-12-19 16:03:09 +0100

    added 'fua112_run_em_all_feb18.py' to test also the atlas for the SRR with 120 cells, to be used in the SHERPA version of feb2018. Also R code to check the differences in the results using the Atlas SRR potency based, and the SRR with 120 cells

---
+++ comparisonAtlasSRR_120clsSRR/20171215_perSNAP_perPrec_aggAreas/main2.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+
+setwd('D:/WORK/projects/1_urbIam/1_CODE_MATLAB/SHERPA/SHERPA-GITHUB/resAtlas/20171215_perSNAP_perPrec_aggAreas')
+
+#read file with results
+#sherpaAtlas is the case used in the Atlas
+#sherpa120cls is the new case to be included in the Feb18 Sherpa
+sherpaAtlas <- read_csv2("20171215_sherpa_chimere_PM25_perSNAP_perPrec_aggAreas.txt", 
+                           skip = 5)
+sherpaAtlas$relative_potential <- as.numeric(sherpaAtlas$relative_potential)
+sherpaAtlas$snap <- as.factor(sherpaAtlas$snap)
+
+sherpa120cls <- read_csv2("20171219_sherpa_chimere_noFW_120cls_PM25_perSNAP_perPrec_aggAreas.txt", 
+                         skip = 5)
+sherpa120cls$relative_potential <- as.numeric(sherpa120cls$relative_potential)
+sherpa120cls$snap <- as.factor(sherpa120cls$snap)
+
+#concatenate results
+sherpaFinalResults <- rbind(sherpaAtlas, sherpa120cls)
+
+#loop over cities
+for (city in unique(sherpaFinalResults$target)) {
+  print(paste0('processing ',city))
+  #select useful columns, filter per city, group and summarize per source-snap, ggplot
+  sherpaFinalResults %>%
+    select(model, target, source, snap, precursor, relative_potential) %>%
+    filter(target==city) %>%
+    group_by(model, source, snap) %>%
+    summarise(relativePotential=sum(relative_potential)) %>%
+    ggplot(aes(fill=source, y=relativePotential, x=snap)) + 
+    geom_bar(stat='identity') +
+    facet_grid( ~ model) +
+    labs(title=city) 
+
+  ggsave(paste('./OUTPUT/comparingSRR_atlas_vs_120cells_',city,'.png'))  
+}
+
