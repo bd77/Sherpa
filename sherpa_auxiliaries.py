e7053e93730bd0931bcfa75ad36c52f5ef9e0d9a
emanuela peduzzi <emanuela.peduzzi@gmail.com>
2018-02-20 15:35:06 +0100

    functions used by more than one module are now in the auxiliaries

--- sherpa_auxiliaries.py
+++ sherpa_auxiliaries.py
@@ -6,8 +6,9 @@ auxiliary functions for SHERPA
 '''
 
 from netCDF4 import Dataset
-from numpy import zeros, power, sqrt, array
-
+import numpy as np
+from numpy import zeros, sqrt, array
+import pandas as pd
 # create a dictionary with emission reductions per precursor and nuts
 #--------------------------------------------------------------------
 
@@ -143,6 +144,175 @@ def deltaNOx_to_deltaNO2(delta_conc_nox, base_conc_nox, base_conc_no2):
 #     delta_conc_nox_var[:] = delta_conc_nox
 
     return delta_conc_no2
+
+def read_nc(nc_file):
+    '''
+    NAME
+        Reads SHERPA ncdf file with Python
+    PURPOSE
+        To read matrix data and put them in a multindexed dataframe
+    PROGRAMMER(S)
+        Denise Pernigotti
+    REVISION HISTORY
+        20/02/2018 Emanuela Peduzzi    
+    REFERENCES
+    
+    '''
+
+    nc_data = Dataset(nc_file, 'r')
+    nc_dims = [dim for dim in nc_data.dimensions]
+    nc_vars = [var for var in nc_data.variables]
+    #sometimes the latitude is written just with lat as in model data
+    latname=list(filter(lambda x: x in nc_vars, ['Lat','lat','latitude']))[0]
+    #latname=list(filter (lambda x: 'lat' in x, nc_vars))[0]
+    lats = nc_data.variables[latname][:]
+    lonname=list(filter(lambda x: x in nc_vars, ['Lon','lon','longitude']))[0]
+    #lonname=list(filter (lambda x: 'lon' in x, nc_vars))[0]
+    lons = nc_data.variables[lonname][:]
+    #if there are three dimensional arrays
+    if len(nc_dims)==3:
+        ncz=str(list(set(nc_dims)-set(['latitude','longitude']))[0])
+        nz=range(len(nc_data.dimensions[ncz]))
+        if ncz=='pollutant':
+            strpoll=nc_data.Order_Pollutant
+            nznames=strpoll.split(', ')
+        else:
+            nznames=[ncz +"{:02d}".format(x+1) for x in nz]
+            #nznames=[ncz + s for s in map(str,range(1,len(nc_data.dimensions[ncz])+1))]
+    #create an index with lat and lon
+    #latrep=map(str, np.repeat(lats,len(lons)))
+    #lonrep=map(str, np.tile(lons,len(lats)))
+    #trasform variables arrays in vectors
+    #allvar={'lat_lon':map(lambda (x,y): x+'_'+y, zip(latrep, lonrep))}
+    #create lat and lon info
+    if len(lats.shape)==2 and len(lons.shape)==2:
+        nrow=lats.shape[0]
+        ncol=lats.shape[1]
+        lon=lons.ravel()
+        lat=lats.ravel()
+    else:
+        nrow=len(lats)
+        ncol=len(lons)
+        lon=np.tile(lons,nrow)
+        lat=np.repeat(lats,ncol)
+
+    y=np.repeat(range(1, nrow+1),ncol)
+    x=np.tile(range(1, ncol+1),nrow)
+    row=list(map(str,y))
+    col=list(map(str,x))
+    index_grid=list(map(lambda x: '_'.join(x),list(zip(col,row))))
+
+    allvar={}
+    allvar['coord']=pd.DataFrame(lon,columns=['lon'])
+    allvar['coord']['lat']=lat
+    allvar['coord']['x']=x
+    allvar['coord']['y']=y
+    allvar['coord'].index=index_grid
+    nc_vars.remove(latname)
+    nc_vars.remove(lonname)
+    # added by EPE to deal with the delta_emissions file created byt 
+    # the GUI which has an extra variable 
+    # @todo this condition can be removed in the future if the GUI 
+    # is fixed (20180219)
+    if 'Nsnaps' in nc_vars: 
+        nc_vars.remove('Nsnaps')
+        
+    for var in nc_vars:
+        varnc=nc_data.variables[var][:]
+        if len(nc_dims)==3:
+            allvar[var]=pd.concat(map(lambda sn : pd.Series(varnc[sn].ravel()),nz),axis=1)
+            allvar[var].columns=nznames
+        else:
+            allvar[var]=pd.DataFrame(varnc.ravel())
+            allvar[var].columns=[var]
+        allvar[var].index=index_grid
+        #allvarnc[var]=allvarnc[var].transpose()
+        #index_var = pd.MultiIndex.from_tuples(zip(np.repeat(var,len(nz)),nznames), names=['vars', ncz])
+        #allvar[var].columns=index_var
+    nc_data.close()
+    reform = {(outerKey, innerKey): values for outerKey, innerDict in allvar.items() for innerKey, values in innerDict.items()}
+    df=pd.DataFrame(reform)
+    return df.transpose()
+
+def read_nuts_area(filenuts, calcall=False, nullnut=False, nutsall=None):
+    '''
+    NAME
+        Import info on grid points attribution to nuts or specific area type from ascii file
+    PURPOSE
+        Import info on grid points attribution to nuts or specific area type from ascii file/s.
+        If the file is single then it must contain the column 'Area [km2]' relative to % of the area in the finest nut,
+        this datum will be set to each nut but it will then aggregated for larger nuts when nutsarea will be calculated
+        If the files are two, then each nut will have its own % area for each grid point, then the data will be merged here
+    PROGRAMMER(S)
+        Denise Pernigotti
+    REVISION HISTORY
+        WARNING by EPE: this function does not work andymore as originally 
+        intended because the gridintersect structure has changed
+        for example there is no nullnut  which has the same name for all 
+        cells! the 'rect' par has not been tested.
+    REFERENCES
+    
+    '''   
+    nuts_info_all={}
+    if(filenuts != 'rect'):
+        nuts_def= filenuts +'.txt'
+        nuts_info = pd.read_csv(nuts_def,delimiter="\t")
+        nuts_info=nuts_info.dropna(axis=1,how='all')
+        nutsnames=list(nuts_info.columns[~nuts_info.columns.isin(['POP','COL','ROW','AREA_km2','LAT','LON','CENTROID_X', 'CENTROID_Y', 'PERCENTAGE', 'POPULATION'])])
+        #optional 'nut' comprising all grid points
+        if calcall :
+        #nutsnames.insert(0, 'ALL')
+            nutsnames.insert(0, 'ALL_'+nutsnames[0])
+            nuts_info[nutsnames[0]]=nutsnames[0]
+        nuts_info['grid']=['_'.join(str(i) for i in z) for z in zip(nuts_info['COL'],nuts_info['ROW'])]
+        if 'AREA_km2' in nuts_info.columns:
+            nuts_area=pd.concat(map(lambda p: nuts_info['AREA_km2'],nutsnames),axis=1)
+            #nuts_area.index=nuts_info['grid']
+            nuts_area.columns=nutsnames
+           #nuts_info=nuts_info[nutsnames]
+        else:
+            sys.exit("missing infos on grid cells area per nut")
+
+        #aggregate data for each nut, create a dictionary
+        nut_info_nut={}
+        nut_info={}
+        nullnut_dct={'NUTS_Lv0':'0', # at this level there is no background
+                     'NUTS_Lv1':'1', 
+                     'NUTS_Lv2':'11',
+                     'NUTS_Lv3':'111'}
+        for nut in nutsnames:
+#            nut = 'NUTS_Lv0'
+            #create a multindex
+            index = pd.MultiIndex.from_tuples(list(zip(nuts_info[nut],nuts_info['grid'])), names=['nutname','grid'])
+            nut_info=pd.Series(list(nuts_area[nut]), index=index)
+            nut_info=nut_info.to_frame(name='area')
+            #aggregate data on these nuts if necessary
+            nut_info_nut=nut_info.groupby(level=[0,1]).sum()
+            #find total area
+            grid_area_tot=nut_info_nut.groupby(level=['grid']).sum()
+            nut_info_nut['parea']=nut_info_nut/grid_area_tot
+            nut_info_nut.loc[nut_info_nut['area']==0,'parea']=0.
+            #eventually remove the fillng code
+            if nullnut is True:
+                for nutkey in set(nut_info_nut.index.get_level_values(0)):
+                    if nutkey[2:]==nullnut_dct[nut]:
+#                        print(nutkey)
+                        nut_info_nut=nut_info_nut.drop(nutkey, level='nutname')
+            nuts_info_all[nut]=nut_info_nut
+       
+    else:
+        nuts_rect=nutsall
+        nuts_rect.index=nuts_rect.index.droplevel(level=0)
+        grid_inrect=nuts_rect.index
+        grid_inrect=grid_inrect[coordinates.loc[grid_inrect,'lon']>=rect_coord['ll']['lon']]
+        grid_inrect=grid_inrect[coordinates.loc[grid_inrect,'lon']<=rect_coord['ur']['lon']]
+        grid_inrect=grid_inrect[coordinates.loc[grid_inrect,'lat']>=rect_coord['ll']['lat']]
+        grid_inrect=grid_inrect[coordinates.loc[grid_inrect,'lat']<=rect_coord['ur']['lat']]
+        nuts_rect=nuts_rect.loc[list(grid_inrect)]
+        nuts_rect['nutname'] = 'rect'
+        nuts_rect.set_index('nutname', append=True, inplace=True)
+        nuts_info_all['rect']=nuts_rect.swaplevel(i=-2, j=-1, axis=0)
+    return nuts_info_all
 
 
 
